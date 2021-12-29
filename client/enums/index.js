16332beb380aef8619faccc09b6b73cb3731949f
Douglas Braga <douglasbraga_lp@hotmail.com>
2021-12-29 00:36:07 -0300

    categories

--- client/enums/index.js
+++ client/enums/index.js
@@ -29,4 +29,26 @@ export const bugs = [
   }
 ]
 
-export const categories = [{ id: 1, name: 'Coleoptera' }]
+export const categories = [
+  { id: 1, name: 'Coleoptera' },
+  { id: 2, name: 'Lepidoptera' },
+  { id: 3, name: 'Hymenoptera' },
+  { id: 4, name: 'Diptera' },
+  { id: 5, name: 'Homoptera' },
+  { id: 6, name: 'Hemiptera' },
+  { id: 7, name: 'Orthoptera' },
+  { id: 8, name: 'Odonata' },
+  { id: 9, name: 'Neutoptera' },
+  { id: 10, name: 'Blattodea' },
+  { id: 11, name: 'Thysanoptera' },
+  { id: 12, name: 'Mallophaga' },
+  { id: 13, name: 'Phasmatodea' },
+  { id: 14, name: 'Mantodea' },
+  { id: 15, name: 'Isoptera' },
+  { id: 16, name: 'Ephemeroptera' },
+  { id: 17, name: 'Dermaptera' },
+  { id: 18, name: 'Siphonaptera' },
+  { id: 19, name: 'Psocptera' },
+  { id: 20, name: 'Thysanura' },
+  { id: 21, name: 'Anoplura' }
+]
