2312c73f96d4318e2204dd4ea37d10964c4e4193
douglas braga <douglas@codebit.com.br>
2022-11-20 17:03:07 -0300

    sentry config

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import './index.css'
 import App from './App'
 // import reportWebVitals from './reportWebVitals'
+
+const { REACT_APP_SENTRY_DNS } = process.env
+
+Sentry.init({
+  dsn: REACT_APP_SENTRY_DNS,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
 
 ReactDOM.render(<App />, document.getElementsByTagName('main')[0])
 
